29aec871e17104e72a1508af36ac3ef0f8fe13d6
umeh-promise <umehpromise1000@gmail.com>
2025-03-07 18:32:48 +0100

    refactor: enhance BeforeAfter component with Next.js Image and improve styling in Wrapper

--- app/components/BeforeandAfter.tsx
+++ app/components/BeforeandAfter.tsx
@@ -1,26 +1,30 @@
-import { cn } from "@/app/utils";
+import Image from "next/image";
 import { ImageData } from "../services/medical-services/types/medical-services";
-
+import { cn } from "../utils";
 
 interface BeforeAfterProps {
   items: ImageData[];
+  className?: string;
 }
 
-export default function BeforeAfter({
-  items,
-}: BeforeAfterProps) {
+export default function BeforeAfter({ items, className }: BeforeAfterProps) {
   return (
     <figure
-      className="flex flex-col xs:grid xs:grid-cols-2 gap-[2.4rem]" >
- 
+      className={cn(
+        "grid h-[24rem] grid-cols-2 xl:h-[48rem] xl:gap-[1.8rem]",
+        className,
+      )}
+    >
       {items.map((item, index) => (
-        <div key={index} className="relative">
-          <img
+        <div key={index} className="relative size-full">
+          <Image
             src={item.image}
             alt={item.type}
-            className="w-full h-full max-h-[550px] object-cover"
+            fill
+            unoptimized
+            className="h-full max-h-[550px] w-full object-cover object-center"
           />
-          <span className="absolute top-4 left-4 text-white px-3 py-1 text-3xl bg-black font-medium rounded">
+          <span className="absolute left-4 top-4 rounded bg-black px-3 py-1 text-3xl font-medium text-white">
             {item.type}
           </span>
         </div>
