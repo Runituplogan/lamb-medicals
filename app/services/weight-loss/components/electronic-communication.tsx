d9f0ed293795eed70fc85c41af118f054e798c5a
umeh-promise <umehpromise1000@gmail.com>
2025-03-08 12:55:43 +0100

    fix: removed mobile image for weight loss service component:

--- app/services/weight-loss/components/electronic-communication.tsx
+++ app/services/weight-loss/components/electronic-communication.tsx
@@ -12,7 +12,6 @@ const ElectronicCommunication: React.FC<ElectronicCommunicationProps> = ({
   return (
     <ServiceImageContentLayout
       image={`${data?.image}`}
-      imageMobile="weight-loss/visual-consultaion-3-mobile.png"
       title={`${data?.headerText}`}
     >
       <p className="font-work_sans text-[1.4rem] font-medium leading-[2.4rem] tracking-[0.02rem] text-grey-750 opacity-80 xs:text-md xs:leading-[3rem] md:leading-[3.6rem] lg:text-lg xxl:text-[2.3rem] xxl:leading-[4.5rem]">
