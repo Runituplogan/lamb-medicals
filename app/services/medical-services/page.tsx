369bc66f043411f5e321186a0e2438d0a1c16752
umeh-promise <umehpromise1000@gmail.com>
2025-03-08 12:43:18 +0100

    fix: added responsiveness for medical services

--- app/services/medical-services/page.tsx
+++ app/services/medical-services/page.tsx
@@ -9,7 +9,12 @@ import LaserVeinTherapy from "./components/laser-vein-therapy";
 import LaserNailFungus from "./components/laser-nail";
 import { useMedicalServices } from "@/app/contexts/MedicalServices";
 import Preloader from "@/app/components/Preloader";
-import { FAQsSection, LaserGenesisSection, LaserTreatmentSection, Section } from "./types/medical-services";
+import {
+  FAQsSection,
+  LaserGenesisSection,
+  LaserTreatmentSection,
+  Section,
+} from "./types/medical-services";
 
 const weightLossTabItems = [
   { href: "#concierge-medicine", label: "Concierge Medicine" },
@@ -30,84 +35,56 @@ export default function MedicalServices() {
   }
 
   const heroData: ContentItem | undefined = MedicalServicesData.content?.find(
-    (item: any) => item.type === "hero"
+    (item: any) => item.type === "hero",
   );
 
-    const consierge: any = MedicalServicesData.content?.find(
-      (item: any) => item.type === "section7"
-    );
-  
-    const  laserGenesis:LaserGenesisSection = MedicalServicesData.content?.find(
-      (item: any) => item.type === "section2"
-    );
-  
-    const QandALaserGen:FAQsSection = MedicalServicesData.content?.find(
-      (item: any) => item.type === "section1"
-    );
-   
-    const laserWarts:LaserTreatmentSection = MedicalServicesData.content?.find(
-      (item: any) => item.type === "section4"
-    );
+  const consierge: any = MedicalServicesData.content?.find(
+    (item: any) => item.type === "section7",
+  );
 
-    const QandALaserWart:FAQsSection = MedicalServicesData.content?.find(
-      (item: any) => item.type === "section5"
-    );
+  const laserGenesis: LaserGenesisSection = MedicalServicesData.content?.find(
+    (item: any) => item.type === "section2",
+  );
 
-    const LaserVein:LaserTreatmentSection = MedicalServicesData.content?.find(
-      (item: any) => item.type === "section6"
-    );
+  const QandALaserGen: FAQsSection = MedicalServicesData.content?.find(
+    (item: any) => item.type === "section1",
+  );
 
-    const QandALaserVein:FAQsSection = MedicalServicesData.content?.find(
-      (item: any) => item.type === "section7"
-    );
+  const laserWarts: LaserTreatmentSection = MedicalServicesData.content?.find(
+    (item: any) => item.type === "section4",
+  );
 
-    const LaserNail:LaserTreatmentSection = MedicalServicesData.content?.find(
-      (item: any) => item.type === "section6"
-    );
+  const QandALaserWart: FAQsSection = MedicalServicesData.content?.find(
+    (item: any) => item.type === "section5",
+  );
 
-    const QandALaserNail:FAQsSection = MedicalServicesData.content?.find(
-      (item: any) => item.type === "section7"
-    );
+  const LaserVein: LaserTreatmentSection = MedicalServicesData.content?.find(
+    (item: any) => item.type === "section6",
+  );
 
+  const QandALaserVein: FAQsSection = MedicalServicesData.content?.find(
+    (item: any) => item.type === "section7",
+  );
+
+  const LaserNail: LaserTreatmentSection = MedicalServicesData.content?.find(
+    (item: any) => item.type === "section6",
+  );
+
+  const QandALaserNail: FAQsSection = MedicalServicesData.content?.find(
+    (item: any) => item.type === "section7",
+  );
 
   return (
     <Fragment>
-      <div className="w-full xs:block hidden">
-        <ServicesHero
-          image={`${heroData?.image}`}
-          title={`${heroData?.headerText}`}
-          description={`${heroData?.bodyText}`}
-        />
-      </div>
-      {/* <div className="block w-full xs:hidden py-10 mt-20"> */}
-      <div className="w-full xs:hidden py-10 mt-20 relative h-screen flex flex-col justify-start items-center">
-        {/* <div className="relative size-full" data-aos="fade-right"> */}
-        <img
-          src={`${heroData?.image}`}
-          alt="facial-service-image-mobile"
-          className="absolute inset-0 h-[90vh] object-cover object-center"
-        />
-        {/* </div> */}
-
-        <div className="grid space-y-[2.4rem] pr-[4rem] text-white md:w-[65rem] px-8 py-36">
-          <h2
-            className="font-rubik text-[32px] font-semibold leading-[3rem] w-full text-center"
-            data-aos="fade-up"
-          >
-            {`${heroData?.headerText}`}
-          </h2>
-          <p
-            className="font-work_sans font-medium leading-[3.5rem] tracking-[0.02em] opacity-80 text-sm text-center"
-            data-aos="fade-left"
-          > 
-            {`${heroData?.bodyText}`}
-          </p>
-        </div>
-      </div>
+      <ServicesHero
+        image={`${heroData?.image}`}
+        title={`${heroData?.headerText}`}
+        description={`${heroData?.bodyText}`}
+      />
       <ServicesTab tabItems={weightLossTabItems} />
-      <ConciergeFamily data={consierge}/>
-      <LaserGenesis data={laserGenesis} questions={QandALaserGen}/>
-      <LaserWartRemoval data={laserWarts} questions={QandALaserWart}/>
+      <ConciergeFamily data={consierge} />
+      <LaserGenesis data={laserGenesis} questions={QandALaserGen} />
+      <LaserWartRemoval data={laserWarts} questions={QandALaserWart} />
       <LaserVeinTherapy data={LaserVein} questions={QandALaserVein} />
       <LaserNailFungus data={LaserNail} questions={QandALaserNail} />
     </Fragment>
