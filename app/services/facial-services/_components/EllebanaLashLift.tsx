70d854d5f927789edcb24cb59f18f19dd69e16e4
umeh-promise <umehpromise1000@gmail.com>
2025-03-08 12:42:55 +0100

    fix: added responsiveness for facial srvices

--- app/services/facial-services/_components/EllebanaLashLift.tsx
+++ app/services/facial-services/_components/EllebanaLashLift.tsx
@@ -3,25 +3,25 @@ import Image from "next/image";
 import ServicesIntro from "../../components/services-intro";
 import { EllebanaLashLiftType } from "../types/facialServicesCustomType";
 
-interface  EllebanaLashListProps {
-  data?:EllebanaLashLiftType
+interface EllebanaLashListProps {
+  data?: EllebanaLashLiftType;
 }
-const EllebanaLashList:React.FC<EllebanaLashListProps>=({data})=> {
-  console.log(data?.image)
+const EllebanaLashList: React.FC<EllebanaLashListProps> = ({ data }) => {
+  console.log(data?.image);
   return (
     <Wrapper id="ellebana-lash-lift" className="grid gap-[4rem] pb-16">
-         {/* <ServicesIntro
+      {/* <ServicesIntro
         title="Ellebana Lash Lift"
         description="With our re-gen formula boasting amino acids, peptides, and plant based ingredients; Elleebana Lash Lift strengthens, nourishes, and protects lashes. Elleebana Lash Lift provides a sensational, natural enhancement to your eyelashes.   "
       /> */}
-         <ServicesIntro
+      <ServicesIntro
         title={`${data?.headerText}`}
         description={`${data?.bodyText}`}
         btnLink={`/schedule-online`}
         btnText={`${data?.cta_button.text}`}
       />
-         <figure
-        className="relative h-[62.3rem] overflow-hidden rounded md:block hidden"
+      <figure
+        className="relative hidden h-[62.3rem] overflow-hidden rounded md:block"
         data-aos="zoom-in"
       >
         {/* <Image
@@ -47,60 +47,58 @@ const EllebanaLashList:React.FC<EllebanaLashListProps>=({data})=> {
       /> */}
       <div className="w-full space-y-[2rem]">
         <div className="space-y-[2rem] pt-[2rem]" data-aos="fade-left">
-          <h3 className="font-rubik text-[32px] xs:text-[46.57px] font-semibold leading-[6rem] text-grey-200">
+          <h3 className="font-rubik text-[3.2rem] font-semibold leading-[2.4rem] text-grey-200 xs:text-[4rem] lg:text-[5.8rem] lg:leading-[3.2rem] xl:leading-[6rem]">
             {data?.subsections[0].title}
           </h3>
 
-          <div className="grid gap-8 font-work_sans text-[14px] xs:text-[23.28px] font-medium leading-[3.6rem] tracking-[0.02rem] text-grey-750 opacity-80">
-          {data?.subsections[0].bodyText}
+          <div className="grid w-full gap-8 font-work_sans text-[1.4rem] font-medium leading-[2.4rem] tracking-[0.02rem] text-grey-750 text-opacity-80 xs:text-md xs:leading-[3rem] md:leading-[3.6rem] lg:text-lg xxl:text-[2.3rem] xxl:leading-[4.5rem]">
+            {data?.subsections[0].bodyText}
           </div>
         </div>
 
         <div className="space-y-[2rem] pt-[2rem]" data-aos="fade-left">
-          <h3 className="font-rubik text-[32px] xs:text-[46.57px] font-semibold leading-[6rem] text-grey-200">
-          {data?.subsections[1].title}
+          <h3 className="font-rubik text-[3.2rem] font-semibold leading-[2.4rem] text-grey-200 xs:text-[4rem] lg:text-[5.8rem] lg:leading-[4.2rem] xl:leading-[6rem]">
+            {data?.subsections[1].title}
           </h3>
 
-          <div className="grid gap-8 font-work_sans text-[14px] xs:text-[23.28px] font-medium leading-[3.6rem] tracking-[0.02rem] text-grey-750 opacity-80">
-          {data?.subsections[1].bodyText}
+          <div className="grid w-full gap-8 font-work_sans text-[1.4rem] font-medium leading-[2.4rem] tracking-[0.02rem] text-grey-750 text-opacity-80 xs:text-md xs:leading-[3rem] md:leading-[3.6rem] lg:text-lg xxl:text-[2.3rem] xxl:leading-[4.5rem]">
+            {data?.subsections[1].bodyText}
           </div>
         </div>
 
         <div className="space-y-[2rem] pt-[2rem]" data-aos="fade-left">
-          <h3 className="font-rubik text-[32px] xs:text-[46.57px] font-semibold leading-[6rem] text-grey-200">
-          {data?.subsections[2].title}
+          <h3 className="font-rubik text-[3.2rem] font-semibold leading-[6rem] text-grey-200 xs:text-[46.57px]">
+            {data?.subsections[2].title}
           </h3>
 
-          <div className="grid gap-8 font-work_sans text-[14px] xs:text-[23.28px] font-medium leading-[3.6rem] tracking-[0.02rem] text-grey-750 opacity-80">
-          {data?.subsections[2].bodyText}
+          <div className="grid w-full gap-8 font-work_sans text-[1.4rem] font-medium leading-[2.4rem] tracking-[0.02rem] text-grey-750 opacity-80 xs:text-md xs:leading-[3rem] md:leading-[3.6rem] lg:text-lg xxl:text-[2.3rem] xxl:leading-[4.5rem]">
+            {data?.subsections[2].bodyText}
           </div>
         </div>
       </div>
       <div className="py-16">
-        <h3 className="font-rubik text-[32px] xs:text-[40px] font-semibold leading-[6rem] text-grey-200 w-full md:text-center">
-        {data?.before_and_after[0].title}
+        <h3 className="w-full font-rubik text-[3.2rem] font-semibold leading-[6rem] text-grey-200 xs:text-[4rem] md:text-center">
+          {data?.before_and_after[0].title}
         </h3>
 
-        <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-10 mt-16">
-          {
-            data?.before_and_after[0].images.map((item,index)=>
-              <div className="relative" key={index}>
-            <h2 className="font-semibold text-white absolute top-10 right-[50%] xs:text-[47.54px] text-[32px] z-10">
-              {item.type}
-            </h2>
+        <div className="mt-16 grid w-full grid-cols-1 gap-10 md:grid-cols-2">
+          {data?.before_and_after[0].images.map((item, index) => (
+            <div className="relative" key={index}>
+              <h2 className="absolute right-[50%] top-10 z-10 text-[3.2rem] font-semibold text-white xs:text-[47.54px]">
+                {item.type}
+              </h2>
 
-            <img
-              src={`${item?.image}`}
-              alt="eye-before"
-              className="object-cover"
-              data-aos="zoom-in"
-            />
-          </div>
-            )
-          }
-        
+              <img
+                src={`${item?.image}`}
+                alt="eye-before"
+                className="object-cover"
+                data-aos="zoom-in"
+              />
+            </div>
+          ))}
+
           {/* <div className="relative">
-            <h2 className="font-semibold text-white absolute top-10 right-[50%] xs:text-[47.54px] text-[32px] z-10">
+            <h2 className="font-semibold text-white absolute top-10 right-[50%] xs:text-[47.54px] text-[3.2rem] z-10">
               After
             </h2>
             <img
@@ -112,7 +110,7 @@ const EllebanaLashList:React.FC<EllebanaLashListProps>=({data})=> {
           </div> */}
 
           {/* <div className="relative">
-            <h2 className="font-semibold text-white absolute top-10 right-[50%] xs:text-[47.54px] text-[32px] z-10">
+            <h2 className="font-semibold text-white absolute top-10 right-[50%] xs:text-[47.54px] text-[3.2rem] z-10">
               Before
             </h2>
             <img
@@ -124,7 +122,7 @@ const EllebanaLashList:React.FC<EllebanaLashListProps>=({data})=> {
           </div> */}
 
           {/* <div className="relative">
-            <h2 className="font-semibold text-white absolute top-10 right-[50%] xs:text-[47.54px] text-[32px] z-10">
+            <h2 className="font-semibold text-white absolute top-10 right-[50%] xs:text-[47.54px] text-[3.2rem] z-10">
               After
             </h2>
             <img
@@ -138,7 +136,6 @@ const EllebanaLashList:React.FC<EllebanaLashListProps>=({data})=> {
       </div>
     </Wrapper>
   );
-}
+};
 
-
-export default EllebanaLashList
+export default EllebanaLashList;
